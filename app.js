f6aa015c1215da106b6a906c3ccd154f11bf920a
Florin Ionescu <sii76354@adobe.com>
2020-02-19 11:27:06 +0200

    Implementing db connection

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 // 1. import express
-const express = require('express')
+const express = require("express");
 // 2. instantiate express in order to have an express app
-const app = express()
+const app = express();
+// 7. import mongoose
+const mongoose = require("mongoose");
 // 3. allowing to use .env varialbes
-require('dotenv').config()
+require("dotenv").config();
+
+// 8. db connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
 // 4. handling requests for '/'
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 
 // 5. which port we want to run our server
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
 // 6. run the app
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
