81a3bd9f15d53dbd0ca85efb71e6a6f6bc896424
Florin Ionescu <sii76354@adobe.com>
2020-02-20 13:39:22 +0200

    Sign out implemented

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,14 @@ const express = require("express");
 // 1. use express router
 const router = express.Router();
 // 4. import controllers
-const { signup, signin } = require("../controllers/user");
+const { signup, signin, signout } = require("../controllers/user");
 // 5. adding a validator
 const {userSignupValidator} = require('../validator')
 
 // 2. create a route and use the logic from controllers
 router.post("/signup", userSignupValidator ,signup);
 router.post("/signin", signin);
+router.get("/signout", signout);
 
 // 3. export router
 module.exports = router;
