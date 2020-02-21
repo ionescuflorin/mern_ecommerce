93b53aa682b79411d761f8e2ebadec7251c0f316
Florin Ionescu <sii76354@adobe.com>
2020-02-21 14:15:33 +0200

    Implementing product model

--- routes/category.js
+++ routes/category.js
@@ -3,9 +3,13 @@ const express = require("express");
 const router = express.Router();
 // 4. import controllers
 const { create } = require("../controllers/category");
+const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
+const { userById } = require("../controllers/user");
 
 // 2. create a route and use the logic from controllers
-router.post("/category/create", create);
+router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
+
+router.param("userId", userById);
 
 // 3. export router
 module.exports = router;
