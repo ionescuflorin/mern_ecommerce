2a6ff89990bfcd12dea9ce794caa0b5e9facf57e
ionescuflorin <florineugen96@gmail.com>
2020-02-22 15:35:12 +0200

    Category by id and single category

--- routes/category.js
+++ routes/category.js
@@ -2,13 +2,15 @@ const express = require("express");
 // 1. use express router
 const router = express.Router();
 // 4. import controllers
-const { create } = require("../controllers/category");
+const { create, categoryById, read } = require("../controllers/category");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
 // 2. create a route and use the logic from controllers
+router.get('/category/:categoryId', read)
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
 
+router.param('categoryId', categoryById)
 router.param("userId", userById);
 
 // 3. export router
