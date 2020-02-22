93dfa0007936645082a7d6dcb6429c4013db13e8
ionescuflorin <florineugen96@gmail.com>
2020-02-22 15:02:07 +0200

    Product by id middleware and single product

--- routes/product.js
+++ routes/product.js
@@ -2,14 +2,17 @@ const express = require("express");
 // 1. use express router
 const router = express.Router();
 // 4. import controllers
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAdmin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+
 // 2. create a route and use the logic from controllers
+router.get('/product/:productId', read)
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 // 3. export router
 module.exports = router;
